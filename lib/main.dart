91adfe0fc33578192cf89b16e21d3d6585bdedc2
Yuliana Montaño <yulianam@ssait-vostro-3490.sintesissa.com.bo>
2023-03-27 10:47:50 -0400

    App initialization and dark theme impementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/consts/theme_data.dart';
+import 'package:grocery_app/provider/dark_theme_provider.dart';
 import 'package:grocery_app/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: Styles.themeData(true, context),
-        home: const HomeScreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return themeChangeProvider;
+        })
+      ],
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeProvider.getDarkTheme, context),
+            home: HomeScreen());
+      }),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/provider/dark_theme_provider.dart';
+import 'package:grocery_app/services/dark_theme_prefs.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    return Scaffold(
+      body: Center(
+          child: SwitchListTile(
+        title: Text('Theme'),
+        secondary: Icon(themeState.getDarkTheme
+            ? Icons.dark_mode_outlined
+            : Icons.light_mode_outlined),
+        onChanged: (bool value) {
+          setState(() {
+            themeState.setDarkTheme = value;
+          });
+        },
+        value: themeState.getDarkTheme,
+      )),
+    );
   }
 }
