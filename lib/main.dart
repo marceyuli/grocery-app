f6f3150af6ec2ad09516ebf0d2aadd4b7ba08db7
Yuliana Montaño <yulianam@ssait-vostro-3490.sintesissa.com.bo>
2023-03-27 13:52:12 -0400

    Implement the bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/consts/theme_data.dart';
 import 'package:grocery_app/provider/dark_theme_provider.dart';
+import 'package:grocery_app/screens/btm_bar.dart';
 import 'package:grocery_app/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -44,7 +45,7 @@ class _MyAppState extends State<MyApp> {
             debugShowCheckedModeBanner: false,
             title: 'Flutter Demo',
             theme: Styles.themeData(themeProvider.getDarkTheme, context),
-            home: HomeScreen());
+            home: BottomBarScreen());
       }),
     );
   }
