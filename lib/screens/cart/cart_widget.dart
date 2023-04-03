94a56c0c8d4e7b94387618060eac7792721a7114
marceyuli <yulianamarcela200@gmail.com>
2023-04-03 13:30:23 -0400

    Cart widget

--- lib/screens/cart/cart_widget.dart
+++ lib/screens/cart/cart_widget.dart
@@ -1,7 +1,9 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:grocery_app/services/utils.dart';
+import 'package:grocery_app/widgets/heart_btn.dart';
 import 'package:grocery_app/widgets/text_widget.dart';
 
 class CartWidget extends StatefulWidget {
@@ -12,12 +14,18 @@ class CartWidget extends StatefulWidget {
 }
 
 class _CartWidgetState extends State<CartWidget> {
-  final quantityTextController = TextEditingController(text: '1');
+  final quantityTextController = TextEditingController();
   @override
   void initState() {
-    quantityTextController.text = '';
+    quantityTextController.text = '1';
     // TODO: implement initState
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    quantityTextController.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,65 +36,109 @@ class _CartWidgetState extends State<CartWidget> {
     return GestureDetector(
       onTap: () {},
       child: Row(children: [
-        Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  height: size.width * 0.25,
-                  width: size.width * 0.25,
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(12.0)),
-                  child: FancyShimmerImage(
-                      imageUrl:
-                          'https://purepng.com/public/uploads/large/purepng.com-apricotapricotfruitfreshorangeapricotsume-481521304824jpk3y.png',
-                      boxFit: BoxFit.fill),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                child: Row(
                   children: [
-                    TextWidget(
-                      text: 'Title',
-                      color: color,
-                      textSize: 20,
-                      isTitle: true,
+                    Container(
+                      height: size.width * 0.25,
+                      width: size.width * 0.25,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0)),
+                      child: FancyShimmerImage(
+                          imageUrl:
+                              'https://purepng.com/public/uploads/large/purepng.com-apricotapricotfruitfreshorangeapricotsume-481521304824jpk3y.png',
+                          boxFit: BoxFit.fill),
                     ),
-                    const SizedBox(
-                      height: 16.0,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextWidget(
+                          text: 'Title',
+                          color: color,
+                          textSize: 20,
+                          isTitle: true,
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        SizedBox(
+                          width: size.width * 0.3,
+                          child: Row(
+                            children: [
+                              quantityController(
+                                  fct: () {},
+                                  icon: CupertinoIcons.minus,
+                                  color: Colors.red),
+                              Flexible(
+                                flex: 1,
+                                child: TextField(
+                                  controller: quantityTextController,
+                                  keyboardType: TextInputType.number,
+                                  maxLines: 1,
+                                  decoration: const InputDecoration(
+                                      focusedBorder: UnderlineInputBorder(
+                                          borderSide: BorderSide())),
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.allow(
+                                        RegExp('[0-9]')),
+                                  ],
+                                  onChanged: (v) {
+                                    setState(() {
+                                      if (v.isEmpty) {
+                                        quantityTextController.text = '1';
+                                      } else {
+                                        return;
+                                      }
+                                    });
+                                  },
+                                ),
+                              ),
+                              quantityController(
+                                  fct: () {},
+                                  icon: CupertinoIcons.plus,
+                                  color: Colors.green)
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: size.width * 0.3,
-                      child: Row(
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Column(
                         children: [
-                          quantityController(
-                              fct: () {},
-                              icon: CupertinoIcons.minus,
-                              color: Colors.red),
-                          Flexible(
-                            flex: 1,
-                            child: TextField(
-                              controller: quantityTextController,
-                              keyboardType: TextInputType.number,
-                              maxLines: 1,
-                              decoration: const InputDecoration(
-                                  focusedBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide())),
+                          InkWell(
+                            onTap: () {},
+                            child: const Icon(
+                              CupertinoIcons.cart_badge_minus,
+                              color: Colors.red,
+                              size: 20,
                             ),
                           ),
-                          quantityController(
-                              fct: () {},
-                              icon: CupertinoIcons.plus,
-                              color: Colors.green)
+                          SizedBox(
+                            height: 5,
+                          ),
+                          const HeartBtn(),
+                          TextWidget(
+                            text: '\$0.29',
+                            color: color,
+                            textSize: 18,
+                            maxLines: 1,
+                          )
                         ],
                       ),
-                    )
+                    ),
                   ],
-                )
-              ],
-            ))
+                )),
+          ),
+        )
       ]),
     );
   }
