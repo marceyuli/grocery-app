3cec0c5ad497068f771965db0d3008cb91324db4
marceyuli <yulianamarcela200@gmail.com>
2023-05-30 16:25:36 -0400

    Wishlist model and provider added

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:grocery_app/inner%20screens/product_details.dart';
 import 'package:grocery_app/provider/dark_theme_provider.dart';
 import 'package:grocery_app/providers/cart_provider.dart';
 import 'package:grocery_app/providers/products_provider.dart';
+import 'package:grocery_app/providers/wishlist_provider.dart';
 import 'package:grocery_app/screens/auth/forget_password.dart';
 import 'package:grocery_app/screens/auth/login.dart';
 import 'package:grocery_app/screens/auth/register.dart';
@@ -55,6 +56,7 @@ class _MyAppState extends State<MyApp> {
         ),
         ChangeNotifierProvider(create: (_) => ProductsProvider(), ),
         ChangeNotifierProvider(create: (_) => CartProvider(),),
+        ChangeNotifierProvider(create: (_) => WishlistProvider()),
       ],
       child:
           Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
---
+++ lib/providers/wishlist_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:grocery_app/models/wishlist_model.dart';
+
+class WishlistProvider with ChangeNotifier {
+  Map<String, WishlistModel> wishlistItems = {};
+
+  Map<String, WishlistModel> get getWishlistItems {
+    return wishlistItems;
+  }
+
+  void addProductToTheWishlist({required String productId}) {
+    if (wishlistItems.containsKey(productId)) {
+      removeOneItem(productId);
+    } else {
+      wishlistItems.putIfAbsent(
+          productId,
+          () => WishlistModel(
+              id: DateTime.now().toString(), productId: productId));
+    }
+  }
+
+  void removeOneItem(String productId) {
+    wishlistItems.remove(productId);
+    notifyListeners();
+  }
+
+  void clearWishlist() {
+    wishlistItems.clear();
+    notifyListeners();
+  }
+}
